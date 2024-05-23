e3bcaaa94a21508f73380b8166e47c270cc5ac3b
Questrade hacked Projects <delushaan@yahoo.com>
2024-05-23 10:50:15 -0400

    project

---
+++ src/Infra/Models/Messages/IdentityIntelligenceMessage.cs
@@ -0,0 +1,25 @@
+using MediatR;
+
+namespace Infra.Models.Messages;
+
+public class IdentityIntelligenceMessage : IRequest<IdentityIntelligenceResultMessage>
+{
+    public string? AddressCountryCode { get; set; }
+    public string? AddressLine1 { get; set; }
+    public string? AddressLine2 { get; set; }
+    public string? AccountNumber { get; set; }
+    public int? AccountStatusId { get; set; }
+    public string? City { get; set; }
+    public string? CrmUserId { get; set; }
+    public string? DateOfBirth { get; set; }
+    public DateTime? EffectiveDate { get; set; }
+    public string? EnterpriseProfileId { get; set; }
+    public string? FirstName { get; set; }
+    public string Id { get; set; } = default!;
+    public string? LastName { get; set; }
+    public string? NationalId { get; set; }
+    public string PhoneNumber { get; set; } = default!;
+    public string? PostalCode { get; set; }
+    public string? ProfileId { get; set; }
+    public string? State { get; set; }
+}
